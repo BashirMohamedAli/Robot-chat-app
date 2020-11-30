e6a77caa68790fdb9b246082568a157e8dd1c2be
Bashir Mohamed Ali <takursiin@gmail.com>
2020-11-30 00:07:32 -0800

    fist commit

---
+++ src/chatBot/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    question: 'asc',
+    answer: 'Wcs',
+  },
+  {
+    question: 'sthy',
+    answer: 'Alx',
+  },
+  {
+    question: 'iwrn',
+    answer: 'Npd',
+  },
+  {
+    question: 'xlda kwrn',
+    answer: 'M.a',
+  },
+  {
+    question: 'kuma tahy',
+    answer: 'Waxan ahay Bobot',
+  },
+  {
+    question: 'imisa jir ayd thy',
+    answer: '16',
+  },
+  {
+    question: 'waa kuma duraan ali',
+    answer: 'Duran is my dear teacher',
+  },
+  {
+    question: 'waa kuma bashir mohamed',
+    answer: "Full-Stack Developer",
+  },
+];
